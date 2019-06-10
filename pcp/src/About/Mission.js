d3efc52b2a1d1291378c1318cea3afbdb34e1737
Thurein Tint <51431308+timothytint@users.noreply.github.com>
2019-06-10 18:39:09 +0630

    6:38

--- pcp/src/About/Mission.js
+++ pcp/src/About/Mission.js
@@ -7,10 +7,10 @@ class Mission extends Component {
       <div>
         <h1>Our Mission</h1>
         <p>
-
 The Pre-Collegiate Program was founded upon an ambitious vision: 
 to support young people in Myanmar in their intellectual 
 development, in their growth as whole people, and in the pursuit of their practical goals. Our mission is to create a society of educated innovators, leaders, and change-makers who solve important challenges in Myanmar and beyond.</p>
+
       <img src={Image} alt="Students explore Danu Self-Administered Zone on a week-long field trip"  />
       </div>
     )
