dc2fb4bed4ba00be558ea471982cfc76ebb94331
Thurein Tint <51431308+timothytint@users.noreply.github.com>
2019-06-10 12:35:55 +0630

    Save

--- pcp/src/Footer.js
+++ pcp/src/Footer.js
@@ -5,15 +5,15 @@ class Footer extends Component {
   render() {
       return (
           <footer className="expanded row">
-            <h6>Contact
+            <h6>Contact<br/>
 
-Pre-Collegiate Program Yangon
+Pre-Collegiate Program Yangon<br/>
 
-09-730-79-846 or 09-739-01-611
+09-730-79-846 or 09-739-01-611<br/>
 
-21/23, Kon Zay Dan St. (Lower Block)
+21/23, Kon Zay Dan St. (Lower Block)<br/>
 
-Pabedan Township, Yangon, Myanmar 11143
+Pabedan Township, Yangon, Myanmar 11143<br/>
 
 precollegiate@gmail.com
 </h6>
--- pcp/src/Header.js
+++ pcp/src/Header.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 import './Header.css'
+import Logo from './images/Logo_V1.svg'
 
 class Header extends Component {
   render() {
     return (
       <div  className="expanded row header">
+        <img src={Logo} alt="Pre-Collegiate Program Yangon" width="60%"/>
         <div id="Header" className="headline text-center small-12 columns">
-          <h1 id="Header">The Pre-Collegiate Program</h1>
-          <h3 id="Header">of Yangon</h3>
         </div>
       </div>
     )
