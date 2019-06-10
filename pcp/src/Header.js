4d9da4561bb098fd71b6f243141ca0430d4830d2
Thurein Tint <51431308+timothytint@users.noreply.github.com>
2019-06-10 14:45:57 +0630

    2:45 6/10

--- pcp/src/Header.js
+++ pcp/src/Header.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import './Header.css'
-import Logo from './images/Logo_V1.svg'
+import Logo from './images/Logo.svg'
 
 class Header extends Component {
   render() {
     return (
       <div  className="expanded row header">
-        <img src={Logo} alt="Pre-Collegiate Program Yangon" width="60%"/>
+        <img className="img.logo" src={Logo} alt="Pre-Collegiate Program Yangon" width="150 px"/>
+        <h1 className="h1.logo"> The Pre-Collegiate Program of Yangon</h1>
         <div id="Header" className="headline text-center small-12 columns">
         </div>
       </div>
