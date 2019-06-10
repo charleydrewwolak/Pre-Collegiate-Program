f61d8d26746fc58be30b02196edf9866c749d4d5
Thurein Tint <51431308+timothytint@users.noreply.github.com>
2019-06-10 15:34:57 +0630

    3:34

--- pcp/src/Header.js
+++ pcp/src/Header.js
@@ -6,7 +6,7 @@ class Header extends Component {
   render() {
     return (
       <div  className="expanded row header">
-        <h1 className="h1.logo" > The Pre-Collegiate Program of Yangon <img className="img.logo" src={Logo} alt="Pre-Collegiate Program Yangon"  width="150px"/> </h1>
+        <h1> The Pre-Collegiate Program of Yangon <img className="img.logo" src={Logo} alt="Pre-Collegiate Program Yangon"  width="150px"/> </h1>
         <div id="Header" className="headline text-center small-12 columns">
         </div>
       </div>
