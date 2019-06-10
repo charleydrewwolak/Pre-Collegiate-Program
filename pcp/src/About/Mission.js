b45ea64aed3bfff6d33ea8ad7e00da8b20e318ff
Thurein Tint <51431308+timothytint@users.noreply.github.com>
2019-06-10 11:46:28 +0630

    Our Mission

--- pcp/src/About/Mission.js
+++ pcp/src/About/Mission.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
+import Image from './OurMission.jpg' 
 
 class Mission extends Component {
   render() {
     return (
       <div>
-        Mission
+        <h1>Our Mission</h1>
+        <p>
+
+The Pre-Collegiate Program was founded upon an ambitious vision: 
+to support young people in Myanmar in their intellectual 
+development, in their growth as whole people, and in the pursuit of their practical goals. Our mission is to create a society of educated innovators, leaders, and change-makers who solve important challenges in Myanmar and beyond.</p>
+      <img src={Image} alt="Students explore Danu Self-Administered Zone on a week-long field trip"  />
       </div>
     )
   }
--- pcp/src/Footer.js
+++ pcp/src/Footer.js
@@ -5,7 +5,18 @@ class Footer extends Component {
   render() {
       return (
           <footer className="expanded row">
-            <h6>Footer goes here</h6>
+            <h6>Contact
+
+Pre-Collegiate Program Yangon
+
+09-730-79-846 or 09-739-01-611
+
+21/23, Kon Zay Dan St. (Lower Block)
+
+Pabedan Township, Yangon, Myanmar 11143
+
+precollegiate@gmail.com
+</h6>
          </footer>
 
       )
