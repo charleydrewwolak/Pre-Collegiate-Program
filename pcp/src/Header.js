250dd17980478f4729db4112c228cc4638cc5800
Thurein Tint <51431308+timothytint@users.noreply.github.com>
2019-06-11 17:10:15 +0630

    Add Link

--- pcp/src/Header.js
+++ pcp/src/Header.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react'
 import './Header.css'
-import Logo from './images/Logo.svg'
+import Logo from './images/LOGO_NEW.svg'
 
 class Header extends Component {
   render() {
     return (
       <div  className="expanded row header">
         <link href="https://fonts.googleapis.com/css?family=Montserrat:400,600,700&display=swap" rel="stylesheet"/> 
-        <img className="logo" src={Logo} alt="Pre-Collegiate Program Yangon" width="150px"/>
+        <a href="./Homepage.js"><img className="logo" src={Logo} alt="Pre-Collegiate Program Yangon" width="150px"/></a>
         <h1 className="h1"> The Pre-Collegiate Program </h1><h1 className="ygn">of Yangon</h1>
-      </div>
+      </div>  
     )
   }
 }
