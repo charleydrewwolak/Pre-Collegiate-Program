3959b0d93946fbc92532b52276ea58b613077bdc
Thurein Tint <51431308+timothytint@users.noreply.github.com>
2019-06-18 13:31:28 +0630

    Updated Info for Application

--- pcp/src/Admissions/Apply.js
+++ pcp/src/Admissions/Apply.js
@@ -10,9 +10,9 @@ class Apply extends Component {
 
     If you are from Myanmar, have finished the Grade 10 Exam or IGCSE, and are aged 16-20, you are eligible to apply for our next class, which will begin in February 2020. <br/><br/>
 
-    In order to join PCP, we will hold two entrance exams at our campus in downtown Yangon. The first will be on August 12th, 2018 (Sunday) from 10:00 A.M to 3 P.M and the second will be in October. Both entrance exams are the same, so you only need to take it once. The exam lasts all day and tests your English language, critical reading, math, and Myanmar language skills. You don’t need to do anything to prepare, except bring a pencil and a good attitude.</p>
+    In order to join PCP, we will hold two entrance exams at our campus in downtown Yangon. The first will be on the 25th of August (Sunday) from 9:00 AM to 12:00 PM and the second will be in October. Both entrance exams are the same, so you only need to take it once. The exam lasts all day and tests your English language, critical reading, math, and Myanmar language skills. You don’t need to do anything to prepare, except bring a pencil and a good attitude.</p>
 
-    <p>Download the application <a href="https://precollegiateyangon.org/wp-content/uploads/2018/08/PCP-Application-Form-20181.pdf" target="_blank" rel="noopener noreferrer">here</a>.</p>
+    <p>Start your application <a href="https://forms.gle/36CoVymLayjmnNvX8" target="_blank" rel="noopener noreferrer">here</a>.</p>
     <img src={image} alt="PCP Students"/>
       </div>
     )
