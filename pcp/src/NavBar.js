e72b4bba807f8109cf73a84ff5117f5208d0089c
Charley Drew-Wolak <charlzahk@gmail.com>
2019-06-18 12:49:54 +0630

    Adjusted nav bar and added new body.css

--- pcp/src/NavBar.js
+++ pcp/src/NavBar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css'; 
+import './Body.css'
 class NavBar extends Component {
   render() {
     return (
