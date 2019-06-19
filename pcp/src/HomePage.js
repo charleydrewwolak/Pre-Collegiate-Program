8e90c1712d2be2dc45b055829ad9f52cd0715c75
Thurein Tint <51431308+timothytint@users.noreply.github.com>
2019-06-19 13:47:47 +0630

    fixed homepage

--- pcp/src/HomePage.js
+++ pcp/src/HomePage.js
@@ -12,11 +12,12 @@ class HomePage extends Component {
           </div>
             
           <div className="Right">
-            <h2>“Education is not the filling of a pail, but the lighting of a fire.”</h2>
+            <h2>Education is not the filling of a pail, but the lighting of a fire.</h2>
             <h3>—William Butler Yeats</h3>
           </div>
         </div>
-        <div>
+
+        <div className="hp">
           <p className="main">
           Since 2003 the Pre-Collegiate Program has had the singular mission
           to create a society of educated innovators, leaders, and change-makers
@@ -28,9 +29,8 @@ class HomePage extends Component {
           skills is a necessary investment. Nurturing young people that can think deeply,
           critically, and empathetically about an issue, successfully seek out information,
           and persuasively share this information
-          with others will be a great asset to the country as it goes forward.
-          </p>
-          <p className="main">We always ask questions of our students that help them open their minds and their hearts. We push students to look closely at their environment and listen carefully to those who speak with them. We encourage our students to entertain ideas they do not accept. We stimulate our students to integrate their traditional values with those essential in contemporary society. We nurture our students in the hope that some will accept the challenge to become change agents.
+          with others will be a great asset to the country as it goes forward. <br/><br/>
+          We always ask questions of our students that help them open their minds and their hearts. We push students to look closely at their environment and listen carefully to those who speak with them. We encourage our students to entertain ideas they do not accept. We stimulate our students to integrate their traditional values with those essential in contemporary society. We nurture our students in the hope that some will accept the challenge to become change agents.
 
 To become a member of the Pre-Collegiate Program is to embark on a lifelong journey of learning and engagement. For those who choose to take on this challenge, the community of the Program will be with you every step of the way.
 </p>
