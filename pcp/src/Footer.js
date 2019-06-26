74e07d6ab4cd4efc340cd9db076aad4a59f3efb8
Charley Drew-Wolak <charlzahk@gmail.com>
2019-06-26 13:50:43 +0630

    Merge branch 'Tim' of https://github.com/charleydrewwolak/Pre-Collegiate-Program- into Tim

--- pcp/src/Footer.js
+++ pcp/src/Footer.js
@@ -5,25 +5,24 @@ import ig from './images/instagram.png'
 
 class Footer extends Component {
   render() {
-      return (
-          <footer className="expanded row">
-            <div className="info">
-              <h6>Contact<br/>
-              Pre-Collegiate Program Yangon<br/>
-              09-730-79-846 or 09-739-01-611<br/>
-              21/23, Kon Zay Dan St. (Lower Block)<br/>
-              Pabedan Township, Yangon, Myanmar 11143<br/>
-              precollegiate@gmail.com
+    return (
+      <footer className="expanded row">
+        <div className="info">
+          <h6>Contact<br />
+            Pre-Collegiate Program Yangon<br />
+            09-730-79-846 or 09-739-01-611<br />
+            21/23, Kon Zay Dan St. (Lower Block)<br />
+            Pabedan Township, Yangon, Myanmar 11143<br />
+            precollegiate@gmail.com
               </h6>
-            </div>
-            <div className="sm">
-             <a href="https://www.facebook.com/precollegiateprogram/" target="_blank" rel="noopener noreferrer"><img className="fb" src={fb} alt="fb"/></a> 
-             <a href="https://www.instagram.com/pcp_yangon/" target="_blank" rel="noopener noreferrer"><img className="ig" src={ig} alt="ig"/></a>
+        </div>
+        <div className="sm">
+          <a href="https://www.facebook.com/precollegiateprogram/" target="_blank" rel="noopener noreferrer"><img className="fb" src={fb} alt="fb" /></a>
+          <a href="https://www.instagram.com/pcp_yangon/" target="_blank" rel="noopener noreferrer"><img className="ig" src={ig} alt="ig" /></a>
+        </div>
+      </footer>
 
-            </div>
-         </footer>
-
-      )
+    )
   }
 
 }
--- pcp/src/NavBar.js
+++ pcp/src/NavBar.js
@@ -1,73 +1,132 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css'; 
-import './Body.css'
 class NavBar extends Component {
+  constructor(props){
+    super();
+    this.state = { displayMenu: false };
+    this.showDM = this.showDM.bind(this);
+    this.hideDM = this.hideDM.bind(this);
+  }
+  showDM = () => {
+    
+    this.setState({ displayMenu: true }, () => {
+      document.addEventListener('click', this.hideDM);
+      //document.getElementById("ddc").classList.toggle('show');
+    });
+  }
+
+  hideDM (ev) {
+    this.setState({ displayMenu: false }, () => {
+      document.removeEventListener('click', this.hideDM)
+      //document.getElementsByClassName("dropdown-content").classList.remove('show');
+
+    });
+  }
+
   render() {
     return (
       <header>
         <link href="https://fonts.googleapis.com/css?family=Montserrat:400,600,700&display=swap" rel="stylesheet"/> 
         <div className="navbar">
           <div className="dropdown">
-            <button className="dropbtn">About 
+            <button onClick={this.showDM.bind(this, 1)} className="dropbtn">About 
               <i className="fa fa-caret-down"></i>
             </button>
-            <div className="dropdown-content">
+            { this.state.displayMenu ? ( 
+            <div className="dropdown-content" id="ddc">
               <a href="#mission"><Link to="/mission">Mission</Link></a>
               <a href="#history"><Link to="/history">History</Link></a>
               <a href="#alumni"><Link to="/alumni">Alumni</Link></a>
             </div>
+            ):
+            ( 
+              null 
+            )
+            }
           </div>
           <div className="dropdown">
-            <button className="dropbtn">Academics 
+            <button onClick={this.showDM} className="dropbtn">Academics 
               <i className="fa fa-caret-down"></i>
             </button>
-            <div className="dropdown-content">
+            { this.state.displayMenu ? (
+            <div className="dropdown-content" id="ddc">
               <a href="#liber-alarts"><Link to="/liberal-arts">Liberal Arts Education</Link></a>
               <a href="#faculty-and-staff"><Link to="/faculty-and-staff">Our Faculty and Staff</Link></a>
               <a href="#curriculum"><Link to="/curriculum">Our Curriculum</Link></a>
               <a href="#study-abroad"><Link to="/study-abroad">Study Abroad</Link></a>
             </div>
+            ):
+            ( 
+              null 
+            )
+            }
           </div> 
           <div className="dropdown">
-            <button className="dropbtn">Student Life 
+            <button onClick={this.showDM} className="dropbtn">Student Life 
               <i className="fa fa-caret-down"></i>
             </button>
-            <div className="dropdown-content">
+            { this.state.displayMenu ? (
+            <div className="dropdown-content" id="ddc">
               <a href="#sl"><Link to="/studentlife">Student Life</Link></a>
               <a href="#blog"><Link to="/blog">Blog</Link></a>
             </div>
+            ):
+            ( 
+              null 
+            )
+            }
           </div> 
           
           <div className="dropdown">
-            <button className="dropbtn">Admissions 
+            <button onClick={this.showDM} className="dropbtn">Admissions 
               <i className="fa fa-caret-down"></i>
             </button>
-            <div className="dropdown-content">
+            { this.state.displayMenu ? (
+            <div className="dropdown-content" id="ddc">
               <a href="#apply"><Link to="/apply">Enroll</Link></a>
               <a href="#financial-aid"><Link to="/financial-aid">Fees and Financial Aid</Link></a>
             </div>
+            ):
+            ( 
+              null 
+            )
+            }
           </div> 
           <div className="dropdown">
-            <button className="dropbtn">Connect 
+            <button  onClick={this.showDM} className="dropbtn">Connect 
               <i className="fa fa-caret-down"></i>
             </button>
-            <div className="dropdown-content">
+            { this.state.displayMenu ? (
+            <div className="dropdown-content" id="ddc">
               <a href="#partner"><Link to="/partner">Partner With Us</Link></a>
               <a href="#intern"><Link to="/intern">Hire an Intern</Link></a>
               <a href="#join"><Link to="/join">Join our Team</Link></a>
-              <a href="#partnerships"><Link to="/join">Partnerships</Link></a>
+              <a href="#partnerships"><Link to="/partnerships">Partnerships</Link></a>
             </div>
+            ):
+            ( 
+              null 
+            )
+            }
           </div> 
           <div className="dropdown">
-            <button className="dropbtn">Student Resources 
+            <button onClick={this.showDM} className="dropbtn">Student Resources 
               <i className="fa fa-caret-down"></i>
             </button>
-            <div className="dropdown-content">
+            { this.state.displayMenu ? (
+            <div className="dropdown-content" id="ddc">
               <a href="#calendar"><Link to="/calendar">Academic Calendar</Link></a>
             </div>
-          </div> 
+            ):
+            ( 
+              null 
+            )
+            }
+        
+          </div>   
         </div>
+        <div className="line"></div>
       </header>
       
       
