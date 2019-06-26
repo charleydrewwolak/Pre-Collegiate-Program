2ff3afae8fb05b22ce96f6745b556b41f073e723
Charley Drew-Wolak <charlzahk@gmail.com>
2019-06-26 16:17:07 +0630

    Progress? added style to dropdown and tried to make them better

--- pcp/src/NavBar.js
+++ pcp/src/NavBar.js
@@ -1,41 +1,21 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css'; 
+import './Body.css'
 class NavBar extends Component {
-  constructor(props){
-    super(props);
-    this.state = {isToggleOn: false};
-    this.handleClick = this.handleClick.bind(this);
-    this.setWrapperRef = this.setWrapperRef.bind(this);
-    this.handleClickOutside = this.handleClickOutside.bind(this);
-  }
-  componentDidMount() {
-    document.addEventListener('mousedown', this.handleClickOutside);
+  showDM = () => {
+    this.setState(() => {
+      document.addEventListener('click', this.hideDM);
+      document.getElementById("ddc").classList.toggle('show');
+    });
   }
 
-  componentWillUnmount() {
-    document.removeEventListener('mousedown', this.handleClickOutside);
-  }
+  hideDM = (ev) => {
+    this.setState(() => {
+      document.removeEventListener('click', this.hideDM);
+      document.getElementById("ddc").classList.remove('show');
 
-  /**
-   * Set the wrapper ref
-   */
-  setWrapperRef(node) {
-    this.wrapperRef = node;
-  }
-  handleClick() {
-    this.setState(state => ({
-      isToggleOn: !state.isToggleOn,
-    }));
-    document.getElementsByClassName("dropdown-content").classList.toggle("show");
-  }
-  handleClickOutside(event) {
-    if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
-      var myDropdown = document.getElementsByClassName("dropdown-content");
-      if (myDropdown.classList.contains('show')) {
-        myDropdown.classList.remove('show');
-      }
-    }
+    });
   }
 
   render() {
@@ -44,20 +24,20 @@ class NavBar extends Component {
         <link href="https://fonts.googleapis.com/css?family=Montserrat:400,600,700&display=swap" rel="stylesheet"/> 
         <div className="navbar">
           <div className="dropdown">
-            <button onClick={this.handleClick} className="dropbtn">About 
+            <button onClick={this.showDM} className="dropbtn">About 
               <i className="fa fa-caret-down"></i>
             </button>
-            <div className="dropdown-content">
+            <div className="dropdown-content" id="ddc">
               <a href="#mission"><Link to="/mission">Mission</Link></a>
               <a href="#history"><Link to="/history">History</Link></a>
               <a href="#alumni"><Link to="/alumni">Alumni</Link></a>
             </div>
           </div>
           <div className="dropdown">
-            <button onClick={this.handleClick} className="dropbtn">Academics 
+            <button onClick={this.showDM} className="dropbtn">Academics 
               <i className="fa fa-caret-down"></i>
             </button>
-            <div className="dropdown-content">
+            <div className="dropdown-content" id="ddc">
               <a href="#liber-alarts"><Link to="/liberal-arts">Liberal Arts Education</Link></a>
               <a href="#faculty-and-staff"><Link to="/faculty-and-staff">Our Faculty and Staff</Link></a>
               <a href="#curriculum"><Link to="/curriculum">Our Curriculum</Link></a>
@@ -65,29 +45,28 @@ class NavBar extends Component {
             </div>
           </div> 
           <div className="dropdown">
-            <button onClick={this.handleClick} className="dropbtn">Student Life 
+            <button onClick={this.showDM} className="dropbtn">Student Life 
               <i className="fa fa-caret-down"></i>
             </button>
-            <div className="dropdown-content">
+            <div className="dropdown-content" id="ddc">
               <a href="#sl"><Link to="/studentlife">Student Life</Link></a>
               <a href="#blog"><Link to="/blog">Blog</Link></a>
             </div>
           </div> 
-          
           <div className="dropdown">
-            <button onClick={this.handleClick} className="dropbtn">Admissions 
+            <button onClick={this.showDM} className="dropbtn">Admissions 
               <i className="fa fa-caret-down"></i>
             </button>
-            <div className="dropdown-content">
+            <div className="dropdown-content" id="ddc">
               <a href="#apply"><Link to="/apply">Enroll</Link></a>
               <a href="#financial-aid"><Link to="/financial-aid">Fees and Financial Aid</Link></a>
             </div>
           </div> 
           <div className="dropdown">
-            <button  onClick={this.handleClick} className="dropbtn">Connect 
+            <button  onClick={this.showDM} className="dropbtn">Connect 
               <i className="fa fa-caret-down"></i>
             </button>
-            <div className="dropdown-content">
+            <div className="dropdown-content" id="ddc">
               <a href="#partner"><Link to="/partner">Partner With Us</Link></a>
               <a href="#intern"><Link to="/intern">Hire an Intern</Link></a>
               <a href="#join"><Link to="/join">Join our Team</Link></a>
@@ -95,13 +74,13 @@ class NavBar extends Component {
             </div>
           </div> 
           <div className="dropdown">
-            <button onClick={this.handleClick} className="dropbtn">Student Resources 
+            <button onClick={this.showDM} className="dropbtn">Student Resources 
               <i className="fa fa-caret-down"></i>
             </button>
-            <div className="dropdown-content">
+            <div className="dropdown-content" id="ddc">
               <a href="#calendar"><Link to="/calendar">Academic Calendar</Link></a>
             </div>
-          </div> 
+          </div>   
         </div>
         <div className="line"></div>
       </header>
