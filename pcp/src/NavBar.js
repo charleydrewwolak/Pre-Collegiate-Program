c2c88acd34cdff64ae8ec1e6b5fb1183e150ce4a
Charley Drew-Wolak <charlzahk@gmail.com>
2019-06-26 14:00:06 +0630

    Revert "Charley's Desparate Tries on the Navbar"
    This reverts commit ab8c67cfc8577515acfa4eafaf4a4c24d4bbe24e.

--- pcp/src/NavBar.js
+++ pcp/src/NavBar.js
@@ -3,25 +3,39 @@ import { Link } from 'react-router-dom';
 import './NavBar.css'; 
 class NavBar extends Component {
   constructor(props){
-    super();
-    this.state = { displayMenu: false };
-    this.showDM = this.showDM.bind(this);
-    this.hideDM = this.hideDM.bind(this);
+    super(props);
+    this.state = {isToggleOn: false};
+    this.handleClick = this.handleClick.bind(this);
+    this.setWrapperRef = this.setWrapperRef.bind(this);
+    this.handleClickOutside = this.handleClickOutside.bind(this);
   }
-  showDM = () => {
-    
-    this.setState({ displayMenu: true }, () => {
-      document.addEventListener('click', this.hideDM);
-      //document.getElementById("ddc").classList.toggle('show');
-    });
+  componentDidMount() {
+    document.addEventListener('mousedown', this.handleClickOutside);
   }
 
-  hideDM (ev) {
-    this.setState({ displayMenu: false }, () => {
-      document.removeEventListener('click', this.hideDM)
-      //document.getElementsByClassName("dropdown-content").classList.remove('show');
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.handleClickOutside);
+  }
 
-    });
+  /**
+   * Set the wrapper ref
+   */
+  setWrapperRef(node) {
+    this.wrapperRef = node;
+  }
+  handleClick() {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn,
+    }));
+    document.getElementsByClassName("dropdown-content").classList.toggle("show");
+  }
+  handleClickOutside(event) {
+    if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
+      var myDropdown = document.getElementsByClassName("dropdown-content");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+    }
   }
 
   render() {
@@ -30,101 +44,64 @@ class NavBar extends Component {
         <link href="https://fonts.googleapis.com/css?family=Montserrat:400,600,700&display=swap" rel="stylesheet"/> 
         <div className="navbar">
           <div className="dropdown">
-            <button onClick={this.showDM.bind(this, 1)} className="dropbtn">About 
+            <button onClick={this.handleClick} className="dropbtn">About 
               <i className="fa fa-caret-down"></i>
             </button>
-            { this.state.displayMenu ? ( 
-            <div className="dropdown-content" id="ddc">
+            <div className="dropdown-content">
               <a href="#mission"><Link to="/mission">Mission</Link></a>
               <a href="#history"><Link to="/history">History</Link></a>
               <a href="#alumni"><Link to="/alumni">Alumni</Link></a>
             </div>
-            ):
-            ( 
-              null 
-            )
-            }
           </div>
           <div className="dropdown">
-            <button onClick={this.showDM} className="dropbtn">Academics 
+            <button onClick={this.handleClick} className="dropbtn">Academics 
               <i className="fa fa-caret-down"></i>
             </button>
-            { this.state.displayMenu ? (
-            <div className="dropdown-content" id="ddc">
+            <div className="dropdown-content">
               <a href="#liber-alarts"><Link to="/liberal-arts">Liberal Arts Education</Link></a>
               <a href="#faculty-and-staff"><Link to="/faculty-and-staff">Our Faculty and Staff</Link></a>
               <a href="#curriculum"><Link to="/curriculum">Our Curriculum</Link></a>
               <a href="#study-abroad"><Link to="/study-abroad">Study Abroad</Link></a>
             </div>
-            ):
-            ( 
-              null 
-            )
-            }
           </div> 
           <div className="dropdown">
-            <button onClick={this.showDM} className="dropbtn">Student Life 
+            <button onClick={this.handleClick} className="dropbtn">Student Life 
               <i className="fa fa-caret-down"></i>
             </button>
-            { this.state.displayMenu ? (
-            <div className="dropdown-content" id="ddc">
+            <div className="dropdown-content">
               <a href="#sl"><Link to="/studentlife">Student Life</Link></a>
               <a href="#blog"><Link to="/blog">Blog</Link></a>
             </div>
-            ):
-            ( 
-              null 
-            )
-            }
           </div> 
           
           <div className="dropdown">
-            <button onClick={this.showDM} className="dropbtn">Admissions 
+            <button onClick={this.handleClick} className="dropbtn">Admissions 
               <i className="fa fa-caret-down"></i>
             </button>
-            { this.state.displayMenu ? (
-            <div className="dropdown-content" id="ddc">
+            <div className="dropdown-content">
               <a href="#apply"><Link to="/apply">Enroll</Link></a>
               <a href="#financial-aid"><Link to="/financial-aid">Fees and Financial Aid</Link></a>
             </div>
-            ):
-            ( 
-              null 
-            )
-            }
           </div> 
           <div className="dropdown">
-            <button  onClick={this.showDM} className="dropbtn">Connect 
+            <button  onClick={this.handleClick} className="dropbtn">Connect 
               <i className="fa fa-caret-down"></i>
             </button>
-            { this.state.displayMenu ? (
-            <div className="dropdown-content" id="ddc">
+            <div className="dropdown-content">
               <a href="#partner"><Link to="/partner">Partner With Us</Link></a>
               <a href="#intern"><Link to="/intern">Hire an Intern</Link></a>
               <a href="#join"><Link to="/join">Join our Team</Link></a>
               <a href="#partnerships"><Link to="/partnerships">Partnerships</Link></a>
             </div>
-            ):
-            ( 
-              null 
-            )
-            }
           </div> 
           <div className="dropdown">
-            <button onClick={this.showDM} className="dropbtn">Student Resources 
+            <button onClick={this.handleClick} className="dropbtn">Student Resources 
               <i className="fa fa-caret-down"></i>
             </button>
-            { this.state.displayMenu ? (
-            <div className="dropdown-content" id="ddc">
+            <div className="dropdown-content">
               <a href="#calendar"><Link to="/calendar">Academic Calendar</Link></a>
             </div>
-            ):
-            ( 
-              null 
-            )
-            }
-        
-          </div>   
+          </div> 
         </div>
         <div className="line"></div>
       </header>
