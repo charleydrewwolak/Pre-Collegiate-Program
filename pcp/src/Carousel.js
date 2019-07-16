c0687a1ddf88fe6a27b18a151273c4114ba24e2c
Charley <charlzahk@gmail.com>
2019-07-16 14:49:55 +0630

    Got the Carousel working

--- pcp/src/Carousel.js
+++ pcp/src/Carousel.js
@@ -26,14 +26,9 @@ class ControlledCarousel extends Component {
   
       return (
         <div className="carousel">
-            <link
-                rel="stylesheet"
-                href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-                integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-                crossorigin="anonymous"
-                />
+    
         
-        <Carousel
+        <Carousel 
           activeIndex={index}
           direction={direction}
           onSelect={this.handleSelect}
