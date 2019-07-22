f6b5f04e609acf8a39c6b3a997b8b0f260bcc76c
Charley <charlzahk@gmail.com>
2019-07-22 16:30:46 +0630

    Format internships

--- pcp/src/StudentLife/Internships.js
+++ pcp/src/StudentLife/Internships.js
@@ -10,7 +10,7 @@ class Internships extends Component {
         {
           name: "Tagu Films",
           description: "Established in 2013, Tagu Films is composed of local Myanmar filmmakers with international experience. Since its inception, Tagu Films has strived to showcase Myanmar through creative visual storytelling that captures this unique country.",
-          link: ""
+          link: "https://tagufilms.com/"
         },
       ]
     }
@@ -19,17 +19,13 @@ class Internships extends Component {
   render() {
     return (
       <div className="t">
+        <h2>Organizations</h2>
         <table>
-          <tr>
-            <th>Organizations</th>
-          </tr>
-
         { this.state.sites.map(m => (
           <tr>
         
-            <td><img className="logos" src={require(`./${m.name.replace(' ', '').toLowerCase()}.png`)} alt="logo"/></td>
-            <td>{m.name}</td>
-            <td>{m.description}</td>
+            <td><a href={m.link}><img className="logos" src={require(`./${m.name.replace(' ', '').toLowerCase()}.png`)} alt="logo"/></a></td>
+            <td><strong className="title">{m.name}</strong> <br/> {m.description}</td>
           </tr>
         ))}
         </table>
