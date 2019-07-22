5336dbd5f96c725f6212351c1ba73bdf2e11386f
Charley <charlzahk@gmail.com>
2019-07-22 16:12:59 +0630

    Got images to map

--- pcp/src/StudentLife/Internships.js
+++ pcp/src/StudentLife/Internships.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './Table.css'
 
+
 class Internships extends Component {
   constructor(){
     super();
@@ -11,74 +12,6 @@ class Internships extends Component {
           description: "Established in 2013, Tagu Films is composed of local Myanmar filmmakers with international experience. Since its inception, Tagu Films has strived to showcase Myanmar through creative visual storytelling that captures this unique country.",
           link: ""
         },
-        {
-          name: "Doh Eain",
-          description: "DkKDKD meyoowooeo",
-          link: ""
-        },
-        {
-          name: "Impact Hub",
-          description: "DkKDKD meyoowooeo",
-          link: ""
-        },
-        {
-          name: "Association for the Assistance of Political Prisoners",
-          description: "DkKDKD meyoowooeo",
-          link: ""
-        },
-        {
-          name: "Solomon School",
-          description: "DkKDKD meyoowooeo",
-          link: ""
-        },
-        {
-          name: "Instituto Oikos Myanmar",
-          description: "DkKDKD meyoowooeo",
-          link: ""
-        },
-        {
-          name: "Indigo Energy",
-          description: "DkKDKD meyoowooeo",
-          link: ""
-        },
-        {
-          name: "Proximity Designs",
-          description: "DkKDKD meyoowooeo",
-          link: ""
-        },
-        {
-          name: "360Ed",
-          description: "DkKDKD meyoowooeo",
-          link: ""
-        },
-        {
-          name: "Design 2000",
-          description: "DkKDKD meyoowooeo",
-          link: ""
-        },
-        {
-          name: "Yangon Heritage Trust",
-          description: "DkKDKD meyoowooeo",
-          link: ""
-        },
-        {
-          name: "Frontier Magazine",
-          description: "DkKDKD meyoowooeo",
-          link: ""
-        },
-        {
-          name: "MYEO",
-          description: "DkKDKD meyoowooeo",
-          link: ""
-        },
-        {
-          name: "Centre for Economic and Social Development",
-          description: "DkKDKD meyoowooeo",
-        },
-        {
-          name: "Koe Koe Tech",
-          description: "DkKDKD meyoowooeo",
-        }
       ]
     }
   }
@@ -88,13 +21,13 @@ class Internships extends Component {
       <div className="t">
         <table>
           <tr>
-            <th>Organization</th>
-            <th></th>
+            <th>Organizations</th>
           </tr>
 
         { this.state.sites.map(m => (
           <tr>
-            <td><img source={"./".concat(m.name.replace(' ', ''),".png").toLowerCase()} alt="logo"></img></td>
+        
+            <td><img className="logos" src={require(`./${m.name.replace(' ', '').toLowerCase()}.png`)} alt="logo"/></td>
             <td>{m.name}</td>
             <td>{m.description}</td>
           </tr>
