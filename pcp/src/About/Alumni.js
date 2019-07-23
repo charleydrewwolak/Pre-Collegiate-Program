d9a9106c1a51c1e7d5fd4aa5ad31f493132a0550
Charley <charlzahk@gmail.com>
2019-07-23 14:47:12 +0630

    Add Alumni Page

--- pcp/src/About/Alumni.js
+++ pcp/src/About/Alumni.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react'
+import './Alumni.css'
 
 class Alumni extends Component {
+  constructor(){
+    super();
+    this.state = {
+      profiles: [
+        
+        {
+          name: "Kitty Mewow",
+          wave: "1st",
+          bio: "Meow mewo mewo mewow ee oeeehhee megehee jjdjdjd Meow mewo mewo mewow ee oeeehhee megehee jjdjdjd Meow mewo mewo mewow ee oeeehhee megehee jjdjdjd Meow mewo mewo mewow ee oeeehhee megehee jjdjdjd Meow mewo mewo mewow ee oeeehhee megehee jjdjdjd Meow mewo mewo mewow ee oeeehhee megehee jjdjdjd Meow mewo mewo mewow ee oeeehhee megehee jjdjdjd Meow mewo mewo mewow ee oeeehhee megehee jjdjdjd "
+        },
+      ]
+    }
+  }
   render() {
     return (
       <div>
-        Alumni
+        <h2 className="staff">Our Alumni</h2>
+        { this.state.profiles.map(m => (
+          <div className="profile">
+            <img alt="Portrait" className="portrait" src={require(`./AlumniPortraits/${m.name.replace(' ','')}.jpg`)}/>
+            <div className="bio">
+              <div className="title">
+              <strong className="Name">{m.name}</strong>
+              <i>- {m.wave} Wave</i> 
+              </div>
+              <p>{m.bio}</p>
+
+            </div>
+          </div>
+         ))}
       </div>
     )
   }
--- pcp/src/StudentLife/Internships.js
+++ pcp/src/StudentLife/Internships.js
@@ -35,7 +35,7 @@ class Internships extends Component {
         { this.state.sites.map(m => (
           <tr>
         
-            <td className="RB"><a href={m.link}><img className="logos" src={require(`./internshipLogos/${m.name.replace(' ', '').toLowerCase()}.png`)} alt="logo"/></a></td>
+            <td className="RB"><a href={m.link} target="_blank" rel="noopener noreferrer"><img className="logos" src={require(`./internshipLogos/${m.name.replace(' ', '').toLowerCase()}.png`)} alt="logo" /></a></td>
             <td className="LB"><strong className="title">{m.name}</strong> <br/> {m.description}</td>
           </tr>
         ))}
