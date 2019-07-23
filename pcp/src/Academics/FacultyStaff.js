55a39ebbb9ee14010c568e99b348873efb8d6021
Thurein Tint <51431308+timothytint@users.noreply.github.com>
2019-07-23 17:35:38 +0630

    Faculty/Staff Page Edit

--- pcp/src/Academics/FacultyStaff.js
+++ pcp/src/Academics/FacultyStaff.js
@@ -10,19 +10,19 @@ class FacultyStaff extends Component {
         {
           name: "Kristen Hartman",
           title: "Director",
-          bio:"holds an MA and BA in English Literature and has been teaching composition and literature at the university level since 2001. In addition to teaching, she has worked at the Centre for the Study and Teaching of Writing, the Digital Media Project, and as assistant editor for the academic journal “American Periodicals.” Prior to joining the Pre-Collegiate Program of Yangon, Kristen directed the Access Academy at the Asian University for Women."
+          bio:"Kristen holds an MA and BA in English Literature and has been teaching composition and literature at the university level since 2001. In addition to teaching, she has worked at the Centre for the Study and Teaching of Writing, the Digital Media Project, and as assistant editor for the academic journal “American Periodicals.” Prior to joining the Pre-Collegiate Program of Yangon, Kristen directed the Access Academy at the Asian University for Women."
         },
 
         {
           name: "Khaing Ei Mon",
           title: "Dean of Administration",
-          bio: "is a graduate of the Pre-Collegiate Program of Yangon. Upon her graduation from the Asian University for Women in 2014 with a major in Public Health Sciences and a minor in Asian Studies, she joined PCP as the Dean of Administration. "
+          bio: "Khaing Ei Mon was born in the Inle Lake region of Shan State. She is devoted to Liberal Arts education and life-long learning and left the Institute of Medicine I (Yangon) to become a student of the Pre-Collegiate Program in 2008. Upon graduation in 2014 from the Asian University for Women in Chittagong, Bangladesh, Khaing Ei Mon joined the Pre-Collegiate Program of Yangon as Dean of Administration."
         },
 
         {
           name: "Joseph McClellan",
           title: "Philosophy",
-          bio:"earned a PhD from Columbia University’s Department of Religion where he specialized in Indo-Tibetan Buddhist philosophy and continental philosophy. He has taught philosophy, religion, and gender studies at a number of universities. He is author of Trans*am: Cis Men and Trans Women in Love (ThreeL Media), and has published translations from Tibetan and French."
+          bio:"Joseph McClellan joined our faculty in January 2018. He earned his Ph.D. in 2013 from Columbia University’s Department of Religion and has taught philosophy, religion, and gender studies at several universities in the United States and Asia. He has published Tibetan and French translations, as well as his own monograph. In addition to teaching our World Philosophies course, he leads a weekly extracurricular Sutta Studies group at PCP."
         },
         
         {
@@ -35,6 +35,11 @@ class FacultyStaff extends Component {
           name: "Tanmai Vemulapalli",
           title: "Anthropology",
           bio: "Being at PCP has been an incredible growing experience. I feel lucky to be grounded in the community of PCP, with the wonderful students and supportive and intelligent faculty, especially as someone who’s new to Myanmar. Every day, I learn from those around me."
+        },
+        {
+          name: "Zune Nyein Soe",
+          title: "Assisst",
+          bio:"Zune Nyein Soe graduated from the Asian University for Women in 2015, with a major in Environmental Science, and joined the Pre-Collegiate Program in May of 2018 as an assistant to the Dean and Program Director."
         },
       ]
     }
