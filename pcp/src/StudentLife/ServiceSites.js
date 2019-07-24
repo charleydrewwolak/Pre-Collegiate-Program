f8e39432731b0daa8663c54ecc8d0a1ac95ba176
Charley <charlzahk@gmail.com>
2019-07-24 14:17:13 +0630

    heheh

--- pcp/src/StudentLife/ServiceSites.js
+++ pcp/src/StudentLife/ServiceSites.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react'
 
 class ServiceSites extends Component {
+  constructor(){
+    super();
+    this.state = {
+      sites: [
+        'example 1',
+        'example 2',
+        'example 3',
+        'example 4',
+        'example 5',
+        'example 6',
+        'example 7',
+        'example 8',
+        'example 9',
+        'example 10',
+      ]
+    }
+  }
   render() {
     return (
       <div>
-        Service Sites
+        <table>
+          <th>Meow</th>
+          <th>Meosw</th>
+        </table>
       </div>
     )
   }
