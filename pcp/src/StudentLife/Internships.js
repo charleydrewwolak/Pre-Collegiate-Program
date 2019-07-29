b32f2664bc1091270a0d51620633a5bb33d01b15
Thurein Tint <51431308+timothytint@users.noreply.github.com>
2019-07-29 17:38:25 +0630

    Internship Sites Edit

--- pcp/src/StudentLife/Internships.js
+++ pcp/src/StudentLife/Internships.js
@@ -13,16 +13,26 @@ class Internships extends Component {
           link: "https://www.360ed.org/"
         },
         {
-          name: "AAPP",
-          description: "The Assistance Association for Political Prisoners (Burma), also known as AAPP, is a non-profit human rights organization based in Mae Sot, Thailand. AAPP was founded in 2000 by former political prisoners living in exile on the Thai/Burma border. Since then, the organization has been run by former political prisoners, with two offices being opened inside Burma in 2012, one in Rangoon and the other in Mandalay. AAPP advocates and lobbies for the release of remaining political prisoners and for the improvement of the lives of political prisoners after their release. The various assistance programs for political prisoners and their family members are aimed at ensuring they have access to education, vocational trainings, mental health counseling and healthcare.",
-          link: "https://aappb.org/"
+          name: "Frontier Myanmar",
+          description: "Frontier is a research and advisory firm incorporated in the United Kingdom. Since 2009 the company has undertaken work in some of the world's most exciting and challenging frontier markets.",
+          link: "https://www.frontiermyanmar.com/"
+        },
+        {
+          name: "Proximity Designs",
+          description: "Proximity Designs is a social business in Myanmar. We design and deliver affordable, income-boosting products that complement the entrepreneurial spirit of rural families.",
+          link: "https://proximitydesigns.org/"
         },
         {
           name: "Tagu Films",
           description: "Established in 2013, Tagu Films is composed of local Myanmar filmmakers with international experience. Since its inception, Tagu Films has strived to showcase Myanmar through creative visual storytelling that captures this unique country.",
           link: "https://tagufilms.com/"
-        }
-
+        },
+        {
+          name: "Wildlife Conservation Society Myanmar",
+          description: "The Wildlife Conservation Society saves wildlife and wild places worldwide. We do so through science, global conservation, education and the management of the world's largest system of urban wildlife parks, led by the flagship New York Bronx Zoo.",
+          link: "https://myanmar.wcs.org/"
+        },
+        
       ]
     }
   }
@@ -35,7 +45,6 @@ class Internships extends Component {
         <table>
           {this.state.sites.map(m => (
             <tr>
-
               <td className="RB"><a href={m.link} target="_blank" rel="noopener noreferrer"><img className="logos" src={require(`./internshipLogos/${m.name.replace(' ', '').toLowerCase()}.png`)} alt="logo" /></a></td>
               <td className="LB"><strong className="title">{m.name}</strong> <br /> {m.description}</td>
             </tr>
