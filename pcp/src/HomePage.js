bbd41e65842730d384c804be9af3781691ea1d68
Charley <charlzahk@gmail.com>
2019-07-30 12:49:54 +0630

    Make Publishable

--- pcp/src/HomePage.js
+++ pcp/src/HomePage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './Homepage.css'
 import ControlledCarousel from './Carousel'
+import { Link } from 'react-router-dom'
 
 class HomePage extends Component {
   render() {
@@ -24,6 +25,22 @@ class HomePage extends Component {
 
 To become a member of the Pre-Collegiate Program is to embark on a lifelong journey of learning and engagement. For those who choose to take on this challenge, the community of the Program will be with you every step of the way.
 </p>
+<h2>Program Overview</h2>
+        <div>
+            <p>At the Pre-Collegiate Program, we work to ensure the success of our students and their communities.</p>
+            <ul>
+                <li>Our <Link to="/curriculum">courses </Link>improve critical thinking, analysis, and and communication skills.</li>
+            </ul>
+            <ul>
+                <li>Our field trips around Yangon, our longer excursions to Bagan and rural Myanmar, our <Link to="/partner">guest speakers</Link>, and our <Link to="/internships">internships</Link> help our student broaden their horizons get hands-on experience, and make professional connections.</li>
+            </ul>
+            <ul>
+                <li>Our one-on-one counseling and experience ensure that our students have their best chance of <Link to="/study-abroad">earning admission and scholarships</Link> to some of the best universities in the world.</li>
+            </ul>
+            <ul>
+                <li>Our inquisitive, supportive community of faculty, alumni, and school partners ensure that are students are ready to work as leaders and agents of change when they return to Myanmar <Link to="/alumni">after their studies</Link>.</li>
+            </ul>
+        </div>
         </div>
       </div>
       
