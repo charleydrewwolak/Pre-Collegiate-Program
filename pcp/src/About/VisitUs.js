7fcf90e25ab6262fb12e0887f448bb9112e12997
Charley <charlzahk@gmail.com>
2019-07-30 13:29:03 +0630

    Revert "1"
    This reverts commit b1fa73f2d053c639d2eb3960e5d179e14343b76c.

---
+++ pcp/src/About/VisitUs.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+
+class VisitUs extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Schedule a Visit</h1>
+        <p>Give us a call at 09-730-79-846 or 09-739-01-611</p>
+        <p>Email us at <a href="mailto:precollegiate@gmail.com">precollegiate@gmail.com</a> or message us on our <a href="https://www.facebook.com/precollegiateprogram">Facebook page</a>.<strong>&nbsp;</strong></p>
+        <p>&nbsp;</p>
+        <p>We welcome visitors during normal business hours. If you would like to learn more about PCP, observe a class, or meet the faculty, please stop by our downtown Yangon campus. We are here Monday through Friday, 9:00 to 5:00 pm, and on Saturday mornings from 9:00 to 11:00. We do take regular field trips, so please let us know if you’re coming. Our address is:</p>
+        <p><br/>21/23 1st Floor,&nbsp; Kon Zay Dan St.
+        <br/>Pabedan Township, Yangon, Myanmar 11143</p>
+        <p><iframe className="Map"title="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3820.0190074957145!2d96.15133831394388!3d16.775729924574314!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30c1ec803119e889%3A0xd4fc9e4ca51bc325!2sKonzedan+Street+Pabedan+Township!5e0!3m2!1sen!2smm!4v1458126024503" width="600" height="450" frameborder="0" allowfullscreen="allowfullscreen"></iframe></p>
+      </div>
+    )
+  }
+}
+
+export default VisitUs
+
