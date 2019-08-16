6c8873c3d21a365471bb1e109f5c85f0e79f9391
Charley <charlzahk@gmail.com>
2019-08-16 15:12:38 -0400

    Updates

---
+++ precache-manifest.bcefa526bc9584f6790e54613d6ed279.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6476b178e900736bc108da31ea1d98f1",
+    "url": "/Pre-Collegiate-Program/index.html"
+  },
+  {
+    "revision": "d93a05af78297e7716de",
+    "url": "/Pre-Collegiate-Program/static/css/main.c74b6320.chunk.css"
+  },
+  {
+    "revision": "86181edec8712c4009d0",
+    "url": "/Pre-Collegiate-Program/static/js/2.c40b32d9.chunk.js"
+  },
+  {
+    "revision": "d93a05af78297e7716de",
+    "url": "/Pre-Collegiate-Program/static/js/main.0ca46d10.chunk.js"
+  },
+  {
+    "revision": "b758fa6fb624ab318ace",
+    "url": "/Pre-Collegiate-Program/static/js/runtime~main.94df8ade.js"
+  },
+  {
+    "revision": "57c3458afdff4377d7f08f68acab843c",
+    "url": "/Pre-Collegiate-Program/static/media/1.57c3458a.jpg"
+  },
+  {
+    "revision": "d6c455c1cd9b1367d088e2138c2b9319",
+    "url": "/Pre-Collegiate-Program/static/media/2.d6c455c1.jpg"
+  },
+  {
+    "revision": "c89b9951d36175074437a5c5bae04773",
+    "url": "/Pre-Collegiate-Program/static/media/3.c89b9951.jpg"
+  },
+  {
+    "revision": "4173aa979d3243e2928e3c4085bab468",
+    "url": "/Pre-Collegiate-Program/static/media/360ed.4173aa97.png"
+  },
+  {
+    "revision": "5c6255590f2f62390ee7f5be0568df99",
+    "url": "/Pre-Collegiate-Program/static/media/Apply.5c625559.jpg"
+  },
+  {
+    "revision": "20e98254d6ff721eeb8688c04a2220d8",
+    "url": "/Pre-Collegiate-Program/static/media/AungHein.20e98254.jpg"
+  },
+  {
+    "revision": "9bc2b6450c0b25245832cdf7f1c8ddcf",
+    "url": "/Pre-Collegiate-Program/static/media/Bonner_PCP-Application-Packet-Myanmar-2019-.9bc2b645.pdf"
+  },
+  {
+    "revision": "eedeced2a409db480681f9d1b1d8ab91",
+    "url": "/Pre-Collegiate-Program/static/media/CollegeList.eedeced2.jpg"
+  },
+  {
+    "revision": "a6cdb660292172aae7703193db9a75ae",
+    "url": "/Pre-Collegiate-Program/static/media/DaniellePurkey.a6cdb660.jpg"
+  },
+  {
+    "revision": "f61fc212e6ec8960cc4b168ca2138ddd",
+    "url": "/Pre-Collegiate-Program/static/media/IUA.f61fc212.png"
+  },
+  {
+    "revision": "9b9c94e90b1af5381eac875e47e96be1",
+    "url": "/Pre-Collegiate-Program/static/media/ImpetusFundApplication.9b9c94e9.pdf"
+  },
+  {
+    "revision": "10496ff7e8db4c135e2e13486896cb71",
+    "url": "/Pre-Collegiate-Program/static/media/JosephMcClellan.10496ff7.jpg"
+  },
+  {
+    "revision": "016167962f626330e8b67705d49bdd1a",
+    "url": "/Pre-Collegiate-Program/static/media/KBZ.01616796.png"
+  },
+  {
+    "revision": "b76b959935e9e50863fa4f40ad97c251",
+    "url": "/Pre-Collegiate-Program/static/media/KKT.b76b9599.png"
+  },
+  {
+    "revision": "7b25d2f0d65dcd15b9dfc5331a29840d",
+    "url": "/Pre-Collegiate-Program/static/media/KhaingEi Mon.7b25d2f0.jpg"
+  },
+  {
+    "revision": "8f2560abd02883e430ceaccd53f78083",
+    "url": "/Pre-Collegiate-Program/static/media/KristenHartman.8f2560ab.jpg"
+  },
+  {
+    "revision": "62e2a439656d75bab4647afe7194145e",
+    "url": "/Pre-Collegiate-Program/static/media/LAB.62e2a439.jpg"
+  },
+  {
+    "revision": "0a9f7428b5a348733989afea096577f7",
+    "url": "/Pre-Collegiate-Program/static/media/LOGO_NEW.0a9f7428.svg"
+  },
+  {
+    "revision": "420b1ec2f07ca57792d72b5bf6ae3850",
+    "url": "/Pre-Collegiate-Program/static/media/LiberalArts.420b1ec2.jpg"
+  },
+  {
+    "revision": "62a864b551b86f68e77e8341d8ae97fb",
+    "url": "/Pre-Collegiate-Program/static/media/LinSun Oo.62a864b5.jpg"
+  },
+  {
+    "revision": "76ccb6866c09bcd8690869c15f51e63e",
+    "url": "/Pre-Collegiate-Program/static/media/Marie-LaureLaila Le Guen.76ccb686.jpg"
+  },
+  {
+    "revision": "ab31e2f42545851e76d7c48a3e70b384",
+    "url": "/Pre-Collegiate-Program/static/media/MayThu Khine.ab31e2f4.jpg"
+  },
+  {
+    "revision": "a9c7d923726e0d6a9b57a07f22487fe6",
+    "url": "/Pre-Collegiate-Program/static/media/OurMission.a9c7d923.jpg"
+  },
+  {
+    "revision": "a981bdc0880a300b2d96622154269335",
+    "url": "/Pre-Collegiate-Program/static/media/Partner.a981bdc0.png"
+  },
+  {
+    "revision": "5399769ac3b470ceb89d20d756923c10",
+    "url": "/Pre-Collegiate-Program/static/media/SAT.5399769a.png"
+  },
+  {
+    "revision": "8b538f2e298069ff31fcbf2aef7d6a0a",
+    "url": "/Pre-Collegiate-Program/static/media/Scholarship.8b538f2e.jpg"
+  },
+  {
+    "revision": "b6096a76cbb7fdac95c3af0fbfb28d9b",
+    "url": "/Pre-Collegiate-Program/static/media/TanmaiVemulapalli.b6096a76.jpg"
+  },
+  {
+    "revision": "dc9e5cd2a82589a3402ceb49cd927978",
+    "url": "/Pre-Collegiate-Program/static/media/ZuneNyein Soe.dc9e5cd2.jpg"
+  },
+  {
+    "revision": "9d2f52d87053f67ce5bdb9180f09bf59",
+    "url": "/Pre-Collegiate-Program/static/media/doheain.9d2f52d8.png"
+  },
+  {
+    "revision": "561e273318608e85cc660c94fa47ba6d",
+    "url": "/Pre-Collegiate-Program/static/media/facebook.561e2733.png"
+  },
+  {
+    "revision": "93466a9a7a3ea89631d30bc1f0698f2f",
+    "url": "/Pre-Collegiate-Program/static/media/frontiermyanmar.93466a9a.png"
+  },
+  {
+    "revision": "8ff30e2d2206ca76b226329a99bdeb5d",
+    "url": "/Pre-Collegiate-Program/static/media/gitameit.8ff30e2d.png"
+  },
+  {
+    "revision": "f31f5b1482910c8dae14af68c737a64d",
+    "url": "/Pre-Collegiate-Program/static/media/hpone.f31f5b14.jpg"
+  },
+  {
+    "revision": "f84e6421847d1c9b84e2e938212dd73b",
+    "url": "/Pre-Collegiate-Program/static/media/img.f84e6421.jpg"
+  },
+  {
+    "revision": "337e0864238a54bf6f62e2d437aa72c4",
+    "url": "/Pre-Collegiate-Program/static/media/impetus.337e0864.png"
+  },
+  {
+    "revision": "766fb96142525fa816d7c87ca3371643",
+    "url": "/Pre-Collegiate-Program/static/media/instagram.766fb961.png"
+  },
+  {
+    "revision": "cdb9ed0d8d27ceb45b82f71d61f0682e",
+    "url": "/Pre-Collegiate-Program/static/media/mothi.cdb9ed0d.jpg"
+  },
+  {
+    "revision": "ad3eb07f8747954dfdeca1390e79bcad",
+    "url": "/Pre-Collegiate-Program/static/media/raj.ad3eb07f.jpg"
+  },
+  {
+    "revision": "fc79a1d2405f87c2ac55bd6da3dc193e",
+    "url": "/Pre-Collegiate-Program/static/media/tagufilms.fc79a1d2.png"
+  },
+  {
+    "revision": "22e6d94f08c65914f4121d6da5bfda5e",
+    "url": "/Pre-Collegiate-Program/static/media/zomia.22e6d94f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e79965019e95f175b73cfba4fa902845.js"
+  "/Pre-Collegiate-Program/precache-manifest.bcefa526bc9584f6790e54613d6ed279.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Pre-Collegiate-Program/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
