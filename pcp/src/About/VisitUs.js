f088a3fd6afe31ddb154a15deaee76e20e077432
Charley <charlzahk@gmail.com>
2020-06-08 19:00:01 -0400

    update address

--- pcp/src/About/VisitUs.js
+++ pcp/src/About/VisitUs.js
@@ -10,7 +10,7 @@ class VisitUs extends Component {
         <p>Email us at <a href="mailto:precollegiate@gmail.com">precollegiate@gmail.com</a> or message us on our <a href="https://www.facebook.com/precollegiateprogram">Facebook page</a>.<strong>&nbsp;</strong></p>
         <p>&nbsp;</p>
         <p>We welcome visitors during normal business hours. If you would like to learn more about PCP, observe a class, or meet the faculty, please stop by our downtown Yangon campus. We are generally present Monday through Thursday, 9:30 AM to 4:30 PM and on Saturdays from 10:30 AM to 4 PM. However, we do take regular field trips, so please let us know if you’re coming! Our address is:</p>
-        <p><br/>21/23 1st Floor,&nbsp; Kon Zay Dan St.
+        <p><br/>21/23 1st Floor,&nbsp; Konzedan
         <br/>Pabedan Township, Yangon, Myanmar 11143</p>
         <p><iframe className="Map"title="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1082.1610591700214!2d96.15413647789795!3d16.772245178558247!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x6bb8f106067d7bcc!2sThe+Pre-Collegiate+Program+of+Yangon!5e0!3m2!1sen!2smm!4v1564724533570!5m2!1sen!2smm" width="600" height="450" frameborder="0" allowfullscreen="allowfullscreen"></iframe></p>
       </div>
