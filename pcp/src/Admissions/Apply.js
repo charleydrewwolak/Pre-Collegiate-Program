bf6f3ea9365d5c332130993e96d2926c99ec39d9
Charley <charlzahk@gmail.com>
2020-06-24 15:43:02 -0400

    Add buttons for applications for financial aid and general application and then fix alumni page

--- pcp/src/Admissions/Apply.js
+++ pcp/src/Admissions/Apply.js
@@ -13,7 +13,10 @@ class Apply extends Component {
           In order to apply to join PCP, you must submit an application and then take one of our entrance exams at our campus in downtown Yangon. The first entrance exam in 2019 will be held on the 25th of August (Sunday) from 9:00 AM to 12:00 PM and the second will be in November (date and time TBA). Both entrance exams are the same, so you only need to take it once. If you take the entrance exam in August and are not invited for an interview, you are permitted to take the entrance exam again in November. 
 The exam lasts half a day and tests your English and Myanmar language skills, critical reading and writing skills, and math aptitude. You don’t need to do anything to prepare, except bring a pen and a good attitude. </p>
 
-        <p>Start your application <a href="https://docs.google.com/forms/d/e/1FAIpQLSfh-JrfGGIu5UI6v1M-tsItc4oTosTR2kQ34500c9iZeXNaDQ/viewform" target="_blank" rel="noopener noreferrer">here</a>.</p>
+        <a className="lButton" href="https://docs.google.com/forms/d/e/1FAIpQLSfh-JrfGGIu5UI6v1M-tsItc4oTosTR2kQ34500c9iZeXNaDQ/viewform" target="_blank" rel="noopener noreferrer">Apply Now</a>
+        <p>
+        <br/>
+        </p>
         <img src={image} width="100%" alt="PCP Students in a discussion" />
       </div>
     )
